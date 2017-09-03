1c0133504751c9423e7ebc526914f9a55cfed973
NiroDu <nirodu1219@outlook.com>
2017-09-04 00:34:20 +0800

    webpack_learn

---
+++ source/unrender/webpack_learn/webpack.production.config.js
@@ -0,0 +1,60 @@
+// webpack.production.config.js
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/build",
+        // filename: "bundle-[hash].js"
+        filename: "bundle.js"
+    },
+    // devtool: 'source-map',
+    devtool: 'none',
+    devServer: {
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        inline: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
+            {
+                // test: /\.css$/,
+                test: /(\.scss|\.css)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }, {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader:"sass-loader"
+                    }
+                ]
+                })
+            }
+            ]
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({sourceMap:true}),
+        new ExtractTextPlugin("style.css")
+    ],
+};
