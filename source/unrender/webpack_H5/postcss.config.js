26ff393527bf0ca365d9b21606bcf3b6360d6177
NiroDu <NiroDu1219@outlook.com>
2017-10-30 13:49:06 +0800

    remove unrender

--- source/unrender/webpack_H5/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require("autoprefixer")({
-      remove: false, //对旧代码不删除prefixer（加快编译速度）
-      browsers: ["> 5%", "ie>=9", "Firefox >= 20", "Chrome >= 45"]
-    })
-  ]
-};
--- source/unrender/webpack_H5/project-list.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  active: 'demo',
-  list: ['demo'],
-};
